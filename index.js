bc02eaea69b6c9ceeb9e1b237ff3399b08a47e4a
ShahabAthar25 <shahabathar25@gmail.com>
2021-08-19 22:49:54 +0500

    Conect To Database

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-import express from 'express';
-import mongoose from 'mongoose';
+const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv').config()
 
 const app = express()
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected To Database'))
 
 app.use(express.json())
 
