8ebe97bdd644cc94e0337443cbd4eb6bbceb0a38
ShahabAthar25 <shahabathar25@gmail.com>
2021-08-19 22:37:38 +0500

    Made API

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+const app = express()
+
+app.use(express.json())
+
+app.listen(3000, () => console.log("Server Running At http://localhost:3000"))
